120448b7cb2e5872f7e57dad6bc1e0c14cb5e9c3
KTIRUVEE <KTIRUVEE@LBNL12026.corp.capgemini.com>
2018-04-12 22:44:51 +0200

    Recursion Started

--- src/com/src/geeksforgeeks/recursion/Program001.java
+++ src/com/src/geeksforgeeks/recursion/Program001.java
@@ -1,7 +1,17 @@
 package com.src.geeksforgeeks.recursion;
 
+// Fibonnaci Sequence
 public class Program001 {
+	public static int getFibonnaci(int n) {
+		if(n==0 || n==1)
+			return n;
+		else {
+			int x = getFibonnaci(n-1);
+			int y=getFibonnaci(n-2);
+			return x+y;
+		}
+	}
 	public static void main(String[] args) {
-		
+		System.out.println(getFibonnaci(5));
 	}
 }
--- src/com/src/practice/Program01.java
+++
@@ -1,7 +0,0 @@
-package com.src.practice;
-
-public class Program01 {
-	public static void main(String[] args) {
-		
-	}
-}
